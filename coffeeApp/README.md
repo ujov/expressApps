e959d89ecf9c3f48afffcba7478ec6350188ec86
ujov <ibehrens.dev@gmail.com>
2016-08-30 21:12:52 +0200

    add simple restApi

---
+++ coffeeApp/README.md
@@ -0,0 +1,1 @@
+coffee tally sheet express mongodb app
--- coffeeApp/app.js
+++ coffeeApp/app.js
@@ -3,11 +3,18 @@
  */
 
 var express = require('express');
+
+var http = require('http');
+var bodyParser  = require('body-parser');
+var restApi = require('express/routes/index');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/*', function (req, res, next) {
+    next();
 });
+
+app.use(bodyParser.json());
+app.use('/api',restApi);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
